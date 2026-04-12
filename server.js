b656fd5ebef1235818cf10694537215c03b33672
DarkMaxDev <gustavomaximiliano1@gmail.com>
2026-04-12 08:01:54 -0600

    solucionado

--- server.js
+++ server.js
@@ -11,6 +11,11 @@ app.use(express.json());
 app.use(cors());
 app.use(helmet());
 //app.use(mongoSanitize());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('Conectado a DB'))
@@ -27,4 +32,8 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/animes', require('./routes/anime'));
 app.use('/api/categories', require('./routes/category'));
 
-app.listen(5000, () => console.log('Server running on port 5000'));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
